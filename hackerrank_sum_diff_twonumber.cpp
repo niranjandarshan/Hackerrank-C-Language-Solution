cd0167555a4efefff13a37c4f054d4d716445366
Niranjan <51872323+niranjandarshan@users.noreply.github.com>
2020-06-02 08:59:06 +0530

    Add files via upload

---
+++ hackerrank_sum_diff_twonumber.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{   int m,n,sum,diff;
+    float p,q,sum1,diff1;
+    scanf("%d%d",&n,&m);
+    scanf("%f%f",&p,&q);
+   
+   
+    printf("%d %d",m+n,n-m);
+  
+    printf("\n%.1f %.1f",p+q,p-q);
+
+
+	
+    
+    return 0;
+}
